f0127fa53486a665c06d9605ef60709208ea7584
Domingo Sanz Marti <domingos@workair.ie>
2026-01-17 00:31:59 +0100

    fix(persistence): add missing error helpers to repo

--- go/kit/persistence/postgres/repo.go
+++ go/kit/persistence/postgres/repo.go
@@ -13,6 +13,7 @@ import (
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
 
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/lib/pq"
 
 	"github.com/dosanma1/forge/go/kit/application/repository"
@@ -380,4 +381,23 @@ func btwArgs(a any) []any {
 		args[i] = val.Index(i).Interface()
 	}
 	return args
+}
+
+// -----------------------------------------------------------------------------
+// Errors
+// -----------------------------------------------------------------------------
+
+// ErrDuplicateKey is the Postgres error code for unique constraint violation
+const ErrDuplicateKey = "23505"
+
+func ErrorIs(err error, code string) bool {
+	var pgErr *pgconn.PgError
+	if errors.As(err, &pgErr) && pgErr.Code == code {
+		return true
+	}
+	return false
+}
+
+func NewErrUnknown(err error) error {
+	return apierrors.InternalError(fmt.Sprintf("query failed, please check the database adapter logs, %s", err.Error()))
 }
