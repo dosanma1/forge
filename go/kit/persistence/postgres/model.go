59af27e14abdba8a44d64b3536482a7893140b5b
Domingo Sanz Marti <domingos@workair.ie>
2026-01-17 11:06:51 +0100

    refactor(persistence): rename model fields to use E prefix (EID, ECreatedAt...)

--- go/kit/persistence/postgres/model.go
+++ go/kit/persistence/postgres/model.go
@@ -7,14 +7,10 @@ import (
 	"gorm.io/gorm"
 )
 
-// -----------------------------------------------------------------------------
-// Timestamp Structures
-// -----------------------------------------------------------------------------
-
 type Timestamps struct {
-	CreatedAt_ time.Time      `gorm:"column:created_at;type:timestamp;autoCreateTime:true"`
-	UpdatedAt_ time.Time      `gorm:"column:updated_at;type:timestamp;autoUpdateTime:true"`
-	DeletedAt_ gorm.DeletedAt `gorm:"column:deleted_at;type:timestamp"`
+	ECreatedAt time.Time      `gorm:"column:created_at;type:timestamp;autoCreateTime:true"`
+	EUpdatedAt time.Time      `gorm:"column:updated_at;type:timestamp;autoUpdateTime:true"`
+	EDeletedAt gorm.DeletedAt `gorm:"column:deleted_at;type:timestamp"`
 }
 
 func TimestampFromTimes(createdAt, updatedAt time.Time, deletedAt *time.Time) Timestamps {
@@ -24,38 +20,34 @@ func TimestampFromTimes(createdAt, updatedAt time.Time, deletedAt *time.Time) Ti
 		delAt.Valid = true
 	}
 	return Timestamps{
-		CreatedAt_: createdAt,
-		UpdatedAt_: updatedAt,
-		DeletedAt_: delAt,
+		ECreatedAt: createdAt,
+		EUpdatedAt: updatedAt,
+		EDeletedAt: delAt,
 	}
 }
 
 func (t *Timestamps) CreatedAt() time.Time {
-	return t.CreatedAt_
+	return t.ECreatedAt
 }
 
 func (t *Timestamps) UpdatedAt() time.Time {
-	return t.UpdatedAt_
+	return t.EUpdatedAt
 }
 
 func (t *Timestamps) DeletedAt() *time.Time {
-	if !t.DeletedAt_.Valid {
+	if !t.EDeletedAt.Valid {
 		return nil
 	}
-	return &t.DeletedAt_.Time
+	return &t.EDeletedAt.Time
 }
 
-// -----------------------------------------------------------------------------
-// Base Models
-// -----------------------------------------------------------------------------
-
-type Model struct { // Public because GORM can't read private embedded structs
-	ID_ string `gorm:"column:id;type:uuid;default:uuid_generate_v4();primaryKey"`
+type Model struct {
+	EID string `gorm:"column:id;type:uuid;default:uuid_generate_v4();primaryKey"`
 	Timestamps
 }
 
 func (d *Model) ID() string {
-	return d.ID_
+	return d.EID
 }
 
 func (d *Model) LID() string {
@@ -64,7 +56,7 @@ func (d *Model) LID() string {
 
 func ModelFromResource(r resource.Resource) Model {
 	return Model{
-		ID_:        r.ID(),
+		EID:        r.ID(),
 		Timestamps: TimestampFromTimes(r.CreatedAt(), r.UpdatedAt(), r.DeletedAt()),
 	}
 }
